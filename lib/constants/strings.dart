51a2121ac42b62e99abaa1fb67d8d37a4601fa40
GauthamAsir <gauthamnadar12@gmail.com>
2021-02-06 23:25:03 +0530

    Add Constants.
    Add Strings & Theme File
    
    Signed-off-by: GauthamAsir <gauthamnadar12@gmail.com>

---
+++ lib/constants/strings.dart
@@ -0,0 +1,7 @@
+const String USERS_COLLECTION = "users";
+const String EMAIL_FIELD = "email";
+const String MOBILE_NO_FIELD = "mobile_no";
+const String USER_STRING = "user";
+const String UNITS_STRING = "units";
+const String CATEGORIES_STRING = "categories";
+const String ADMIN_STRING = "admin";
