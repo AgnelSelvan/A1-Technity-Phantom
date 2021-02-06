d136870e3c5b0549949df14b77cdef768788a9f9
GauthamAsir <gauthamnadar12@gmail.com>
2021-02-06 23:39:14 +0530

    Add Theme Notifier
    Signed-off-by: GauthamAsir <gauthamnadar12@gmail.com>

---
+++ lib/theme/theme_notifier.dart
@@ -0,0 +1,15 @@
+import 'package:annaistore/constants/theme.dart';
+import 'package:flutter/material.dart';
+
+class ThemeNotifier with ChangeNotifier {
+  ThemeData _themeData;
+
+  ThemeNotifier(this._themeData);
+
+  getTheme() => _themeData;
+
+  setTheme(ThemeData themeData) async {
+    _themeData = themeData;
+    notifyListeners();
+  }
+}
