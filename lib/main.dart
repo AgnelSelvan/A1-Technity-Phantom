aef07a4e8aa84f00aa1ecf08987019f213054e34
GauthamAsir <gauthamnadar12@gmail.com>
2021-02-06 22:51:06 +0530

    Firebase initializeApp.
    To initialize App before runApp need to ensure widgets are Initialized.
    
    Signed-off-by: GauthamAsir <gauthamnadar12@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:stock_q/views/pages/root_page.dart';
 import 'package:stock_q/services/auth.dart';
 import 'package:stock_q/services/datastore.dart';
+import 'package:stock_q/views/pages/root_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
