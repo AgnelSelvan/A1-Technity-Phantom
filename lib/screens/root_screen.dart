1c79234f1808f4e149bf6bda6b9dd67ebde3959a
Marishwaran99 <marishwaran22@gmail.com>
2021-02-07 15:51:57 +0530

    bottom bar updated

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:stock_q/screens/navigation_screens/home_screen.dart';
 import 'package:stock_q/screens/navigation_screens/profile_screen.dart';
 import 'package:stock_q/utils/universal_variables.dart';
+import 'package:stock_q/widgets/bottom_appbar.dart';
 
 class RootScreen extends StatefulWidget {
   @override
@@ -12,9 +13,11 @@ class RootScreen extends StatefulWidget {
 class _RootScreenState extends State<RootScreen> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int _page = 0;
-
-  final tabs = [HomeScreen(), ProfileScreen()];
-
+  List<BarItem> _bottomBarItem = [
+    BarItem(title: 'Home', iconData: Icons.home),
+    BarItem(title: 'Profile', iconData: Icons.person),
+  ];
+  final List<Widget> _screens = [HomeScreen(), ProfileScreen()];
   @override
   void initState() {
     super.initState();
@@ -29,45 +32,14 @@ class _RootScreenState extends State<RootScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: Variables.lightGreyColor,
-      body: tabs[_page],
-      bottomNavigationBar: BottomNavigationBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          onTap: navigationTapped,
-          currentIndex: _page,
-          iconSize: 19,
-          selectedFontSize: 20,
-          unselectedFontSize: 16,
-          selectedIconTheme: IconThemeData(color: Variables.primaryColor),
-          unselectedIconTheme: IconThemeData(color: Colors.grey),
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                size: 19,
-              ),
-              title: Text(
-                "Home",
-                style: TextStyle(
-                    fontSize: 18,
-                    color: (_page == 0) ? Variables.primaryColor : Colors.grey),
-              ),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person_outline,
-                size: 19,
-              ),
-              title: Text(
-                "Profile",
-                style: TextStyle(
-                    fontSize: 18,
-                    color: (_page == 1) ? Variables.primaryColor : Colors.grey),
-              ),
-            ),
-          ]),
-    );
+        key: _scaffoldKey,
+        backgroundColor: Variables.lightGreyColor,
+        body: _screens[_page],
+        bottomNavigationBar: AnimatedBottomBar(
+            barItems: _bottomBarItem,
+            duration: Duration(milliseconds: 200),
+            onBarTap: (i) {
+              navigationTapped(i);
+            }));
   }
 }
--- lib/widgets/bottom_appbar.dart
+++ lib/widgets/bottom_appbar.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:stock_q/utils/universal_variables.dart';
+
+class BarItem {
+  String title;
+  IconData iconData;
+  BarItem({this.title, this.iconData});
+}
 
 class AnimatedBottomBar extends StatefulWidget {
-  final List<IconData> barItems;
+  final List<BarItem> barItems;
   final Duration duration;
   final Function onBarTap;
   AnimatedBottomBar(
@@ -12,15 +19,15 @@ class AnimatedBottomBar extends StatefulWidget {
   _AnimatedBottomBarState createState() => _AnimatedBottomBarState();
 }
 
-class _AnimatedBottomBarState extends State<AnimatedBottomBar> {
+class _AnimatedBottomBarState extends State<AnimatedBottomBar>
+    with TickerProviderStateMixin {
   int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 48,
-      color: Colors.grey[100],
+    return Material(
+      elevation: 10.0,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
         child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -35,23 +42,46 @@ class _AnimatedBottomBarState extends State<AnimatedBottomBar> {
     for (int i = 0; i < widget.barItems.length; i++) {
       var item = widget.barItems.elementAt(i);
       bool isSelected = selectedIndex == i;
-
-      _barItems.add(GestureDetector(
-        child: Container(
-          width: 48,
-          height: 36,
-          child: Icon(
-            item,
-            size: isSelected ? 28 : 24,
-            color: isSelected ? Colors.black87 : Colors.grey,
-          ),
-        ),
+      _barItems.add(InkWell(
+        hoverColor: Colors.grey[200],
+        splashColor: Colors.grey[200],
         onTap: () {
           setState(() {
             selectedIndex = i;
             widget.onBarTap(i);
           });
         },
+        child: AnimatedContainer(
+          padding: EdgeInsets.all(8.0),
+          duration: widget.duration,
+          decoration: BoxDecoration(
+              color: isSelected ? Colors.grey[200] : Colors.transparent,
+              borderRadius: BorderRadius.circular(24.0)),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                item.iconData,
+                color: isSelected ? Variables.primaryColor : Colors.black,
+              ),
+              SizedBox(
+                width: 4.0,
+              ),
+              AnimatedSize(
+                curve: Curves.easeInOut,
+                child: Text(
+                  isSelected ? item.title : "",
+                  style: TextStyle(
+                      color: Variables.primaryColor,
+                      fontSize: 14,
+                      letterSpacing: 1.25,
+                      fontWeight: FontWeight.w600),
+                ),
+                duration: widget.duration,
+                vsync: this,
+              ),
+            ],
+          ),
+        ),
       ));
     }
 
